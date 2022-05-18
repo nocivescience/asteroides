b23a247ffc0f16e8c273a176d7bab915a1f12e55
noceive <nocivescience@gmail.com>
2022-05-18 15:43:41 -0400

    creando la nave estatica y colores  en asteroides

--- asteroides.js
+++ asteroides.js
@@ -2,12 +2,14 @@ const gameEl = document.getElementById('game');
 const ctx = gameEl.getContext('2d');
 gameEl.width = window.innerWidth;
 gameEl.height = window.innerHeight;
-function newAsteroid(x,y,r){
+const asteroidColors = ['#F00','#0F0','#00F','#FF0','#0FF','#F0F'];
+function newAsteroid(x,y,r,color){
     var lvlMult = 3+Math.floor(Math.random()*10);
     var roid = {
         x,
         y,
         r,
+        color,
         vx: Math.random()<.5?-1:1,
         vy: Math.random()<.5?-1:1,
         a:Math.random()*Math.PI*2,
@@ -31,11 +33,75 @@ function drawAsteroid(roid){
     ctx.moveTo(roid.x+Math.cos(roid.a)*roid.r,roid.y+Math.sin(roid.a)*roid.r);
     for(var i=0;i<roid.vert;i++){
         var a = roid.a+i*2*Math.PI/roid.vert;
-        ctx.strokeStyle = 'white';
+        ctx.strokeStyle = asteroidColors[i%asteroidColors.length];
+        ctx.fillStyle = asteroidColors[i%asteroidColors.length];
         ctx.lineTo(roid.x+Math.cos(a)*(roid.r+roid.offs[i]),roid.y+Math.sin(a)*(roid.r+roid.offs[i]));
     }
     ctx.closePath();
     ctx.stroke();
+    ctx.fill();
+}
+function moveAsteroids(roids){
+    for(var i=0;i<roids.length;i++){
+        drawAsteroid(roids[i]);
+        roids[i].x += roids[i].vx;
+        roids[i].y += roids[i].vy;
+        if(roids[i].x<0){
+            roids[i].x = gameEl.width;
+        }
+        if(roids[i].x>gameEl.width){
+            roids[i].x = 0;
+        }
+        if(roids[i].y<0){
+            roids[i].y = gameEl.height;
+        }
+        if(roids[i].y>gameEl.height){
+            roids[i].y = 0;
+        }
+    }
+}
+function drawShip(){
+    ship={
+        x:gameEl.width/2,
+        y:gameEl.height/2,
+        r:10,
+        a:Math.PI/2,
+        vx:5,
+        vy:5,
+    }
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'white';
+    ctx.moveTo(ship.x,ship.y);
+    ctx.lineTo(ship.x+Math.cos(ship.a)*ship.r,ship.y+Math.sin(ship.a)*ship.r);
+    ctx.lineTo(ship.x+Math.cos(ship.a+Math.PI/2)*ship.r,ship.y+Math.sin(ship.a+Math.PI/2)*ship.r);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+}
+function moveShip(){
+    drawShip();
+    ship.x += ship.vx;
+    ship.y += ship.vy;
+    if(ship.x<0){
+        ship.x = gameEl.width;
+    }
+    if(ship.x>gameEl.width){
+        ship.x = 0;
+    }
+    if(ship.y<0){
+        ship.y = gameEl.height;
+    }
+    if(ship.y>gameEl.height){
+        ship.y = 0;
+    }
 }
 const roids = createAsteroidBelt();
-roids.forEach(drawAsteroid);
+function update(){
+    ctx.clearRect(0,0,gameEl.width,gameEl.height);
+    moveAsteroids(roids);
+    roids.forEach(drawAsteroid);
+    moveShip();
+    requestAnimationFrame(update);
+}
+update();
